e6f0f2316480e5565bd8ac72751e529984fb4bdf
elloramir <cripboygit@gmail.com>
2025-06-07 18:03:49 -0300

    refactor: changing API to the PDF specs

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pedidos/', include('pedidos.urls')),  # nested API versioning and app
+
+    # Oauth login routes
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Pedidos API route append
+    path('api/v1/pedidos/', include('pedidos.urls')),
 ]
--- src/pedidos/urls.py
+++ src/pedidos/urls.py
@@ -1,23 +1,10 @@
 from django.urls import path, re_path, include
 from rest_framework import routers
 from pedidos.viewsets import PedidoViewSet
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 
 router = routers.DefaultRouter()
 router.register('pedidos', PedidoViewSet, basename='pedido')
 
-schema_view = get_schema_view(
-    openapi.Info(title="API Documentation", default_version='v1'),
-    public=True,
-)
-
 urlpatterns = [
-    path('', include(router.urls)),  # pedidos routes
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', include(router.urls)),
 ]
